70d8c4b76d2f150f5fb9d48e58473b35aea58fb2
Thaís <thaisnl@alu.ufc.br>
2019-02-12 22:54:14 -0300

    Teste

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 //seta o mongoose pra usar promises e define a library da promise
+//blabla teste
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
